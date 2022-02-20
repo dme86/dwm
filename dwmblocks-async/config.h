013ba301be3288313a9b977446050ad76a52646e
Dan <me.daniel.meier@gmail.com>
2022-02-20 15:13:05 +0100

    show battery only if not charging

--- dwmblocks-async/config.h
+++ dwmblocks-async/config.h
@@ -3,6 +3,7 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
+	BLOCK("~/dwm/dwmblocks-async/scripts/battery", 10, 21),
 	BLOCK("~/dwm/dwmblocks-async/scripts/weather", 1800, 22),
 	BLOCK("date +'%a, %d %b [%H:%M:%S]'",    1,    23),
 	BLOCK("~/dwm/dwmblocks-async/scripts/kernel", 86400, 24),
