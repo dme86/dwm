ea9e5005e98b62acf74623830f1f1046630fb818
Dan <me.daniel.meier@gmail.com>
2022-02-20 10:37:07 +0100

    switch to async blocks

---
+++ dwmblocks-async/README.md
@@ -0,0 +1,2 @@
+# dwmblocks-async
+A modular statusbar for `dwm` written in C. You may think of it as `i3blocks`, but for `dwm`.
---
+++ dwmblocks-async/config.h
@@ -0,0 +1,8 @@
+#define CMDLENGTH 60
+#define DELIMITER "  "
+#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+	BLOCK("date",    1,    24),
+	BLOCK("~/dwm/dwmblocks/scripts/pacupdate", 1, 24)
+};
