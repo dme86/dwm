1b7f8a219c1e10eec02ad884469f4517bc782be6
Dan <me.daniel.meier@gmail.com>
2022-02-20 12:48:10 +0100

    add weather script

--- dwmblocks-async/config.h
+++ dwmblocks-async/config.h
@@ -3,6 +3,7 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
+	BLOCK("~/dwm/dwmblocks-async/scripts/weather", 1800, 22),
 	BLOCK("date +'%a, %d %b [%H:%M:%S]'",    1,    23),
 	BLOCK("~/dwm/dwmblocks-async/scripts/kernel", 86400, 24),
 	BLOCK("~/dwm/dwmblocks-async/scripts/pacupdate", 3600, 25)
