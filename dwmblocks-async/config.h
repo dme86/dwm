24f2facc7e8669fcf4f20807e7834cf0a868b471
Dan <me.daniel.meier@gmail.com>
2022-02-24 16:55:32 +0100

    add dynamic if list

--- dwmblocks-async/config.h
+++ dwmblocks-async/config.h
@@ -4,6 +4,7 @@
 
 const Block blocks[] = {
 	BLOCK("~/dwm/dwmblocks-async/scripts/battery", 10, 21),
+        BLOCK("~/dwm/dwmblocks-async/scripts/internet", 15, 20),
 	BLOCK("~/dwm/dwmblocks-async/scripts/weather", 1800, 22),
 	BLOCK("date +'%a, %d %b [%H:%M:%S]'",    1,    23),
 	BLOCK("~/dwm/dwmblocks-async/scripts/kernel", 86400, 24),
