f70bcbb78b0f2905e79aff8491c5f60e67b705bd
Dan <me.daniel.meier@gmail.com>
2024-01-28 20:07:38 +0100

    chore(add): alacritty
    I have decided to make Alacritty my primary terminal once again. The main reason for this switch is the smoother handling of scrolling and marking longer text simultaneously. Additionally, Alacritty now utilizes TOML instead of YAML for its configuration, a change that I find enjoyable to explore and experiment with.

--- config.h
+++ config.h
@@ -8,9 +8,7 @@ static const Gap default_gap        = {.isgap = 1, .realgap = 0, .gappx = 0};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-//static const char *fonts[]          = { "NotoSans Nerd Font:style=Regular:size=17" };
 static const char *fonts[]          = { "Hack Nerd Font:style=Regular:size=17" };
-//static const char dmenufont[]       = "NotoSans Nerd Font:style=Regular:pixelsize=19";
 static const char dmenufont[]       = "Hack Nerd Font:style=Regular:size=19";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -79,7 +77,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray1, "-sf", col_white, NULL };
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *chromium[]  = { "chromium", NULL };
 /* volume control */
 static const char *mutecmd[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
