3a8dc1dde69fa5164c40d471db5b8928a5023428
Dan <me.daniel.meier@gmail.com>
2024-01-28 21:40:41 +0100

    feat(add): un/mute script
    I've created a basic script to showcase a message using Dunst (https://wiki.archlinux.org/title/Dunst).

--- config.h
+++ config.h
@@ -80,7 +80,7 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *chromium[]  = { "chromium", NULL };
 /* volume control */
-static const char *mutecmd[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
+static const char *mutecmd[] = { "/home/dan/.config/dwm/scripts/audio", NULL };
 static const char *volupcmd[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
 static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL };
 /* media keys */
