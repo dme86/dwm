f5438040ea44dc384cff5689e67bff16f957fc23
Dan <me.daniel.meier@gmail.com>
2024-02-27 11:54:55 +0100

    feat(add): incognito on tag
    This feature introduces a tag rule (https://dwm.suckless.org/customisation/tagmask/) for Chromium, ensuring that an incognito tab will consistently appear on its designated tag rather than being tiled on the same tag as the main Chromium window.

--- config.h
+++ config.h
@@ -36,7 +36,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { " ", "爵 ", " ", " "};
+static const char *tags[] = { "", "爵", "󰗹", " "};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -44,8 +44,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Chromium", "chromium", "New Tab - Chromium", 1 << 1, 0, -1 },
+	{ "Chromium", "chromium", "New Incognito Tab - Chromium", 1 << 2, 0, -1 },
 };
 
 /* layout(s) */
